ce81703ac42dc2744023eb2bfbf391fa7c961b37
s1132234 <s1132234@pu.edu.tw>
2026-03-10 12:10:23 +0800

    week3_py

---
+++ test1.py
@@ -0,0 +1,22 @@
+def square(y):
+	print(f"{y}的三次方是{y**3}")
+
+def addnum(z):
+	num = 0
+	for i in range(1,z+1):
+		num+=i
+	return num
+
+
+
+if __name__ == "__main__":
+	x = int(input("請輸入一個整數:"))
+	#x += 10
+
+	if (x<=0):
+		print(f"您輸入的值是{x},小於等於0")
+	else:
+		print(f"您輸入的值是{x},大於0")
+		for i in range(1, x+1):
+			#print(i, end=";")
+			square(i)
---
+++ use3.py
@@ -0,0 +1,8 @@
+from mis import mis2a,mis2b
+from test1 import addnum
+
+mis2a()
+mis2b()
+
+x = int(input())
+print(addnum(x))
